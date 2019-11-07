8679cdc7a793b283b7cb82c537b25b46dddd6a19
ameraibrahim <ameraibrahim09@gmail.com>
2019-11-07 16:18:21 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+   // state can only be accessed in class-based components!
+  state = {
+    persons: [
+      {name: 'Max', age:28},
+      {name: 'Manu', age:29},
+      {name: 'Stephanie', age:26},
+    ]
+  }
+
+  // Needs to be implemented in class-based components! Needs to return some JSX!
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App</h1>
+       <p>This is really working.</p>
+       <button>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies:Racing</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+    //return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi, I\'m a React App'));
+  }
+} 
+
+export default App;
